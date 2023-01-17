5992b7be4eedd6aef4918f5cac6e83213583c028
mitia.mackarevich@yandex.ru <mitia.mackarevich@yandex.ru>
2023-01-17 18:21:53 +0300

    fix: Добавлена пропущенная аннотация

--- general-requester/src/main/java/com/dmakarevich/yellow_collector/general_requester/services/statistics/ReportsStatisticsService.java
+++ general-requester/src/main/java/com/dmakarevich/yellow_collector/general_requester/services/statistics/ReportsStatisticsService.java
@@ -2,6 +2,6 @@ package com.dmakarevich.yellow_collector.general_requester.services.statistics;
 
 public interface ReportsStatisticsService {
 
-    public void getSummaryStatistics();
+    void getSummaryStatistics();
 
 }
--- general-requester/src/main/java/com/dmakarevich/yellow_collector/general_requester/services/statistics/ReportsStatisticsServiceImpl.java
+++ general-requester/src/main/java/com/dmakarevich/yellow_collector/general_requester/services/statistics/ReportsStatisticsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.dmakarevich.yellow_collector.general_requester.services.statistics;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReportsStatisticsServiceImpl implements ReportsStatisticsService{
 
     @Override
